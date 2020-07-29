df51671251f6fa37fa3a84cc023a21d76bf264f7
Igor Souza <igor.souza@ricoy.com.br>
2020-07-29 17:57:54 -0300

    Delete produtos angular

--- projeto01/frontend/src/app/app-routing.module.ts
+++ projeto01/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from  './views/home/home.component'
+import {ProductCrudComponent}  from './views/product-crud/product-crud.component'
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path:"",
+    component:HomeComponent
+},{
+    path:"products",
+    component:ProductCrudComponent
+},{
+    path:"products/create",
+    component:ProductCreateComponent
+},{
+    path:"products/update/:id",
+    component:ProductUpdateComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projeto01/frontend/src/app/app.module.ts
+++ projeto01/frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -11,13 +12,45 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
 
 import {MatSidenavModule} from '@angular/material/sidenav'
-import {MatListModule} from '@angular/material/list'
+import {MatCardModule} from '@angular/material/card'
+import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component'
+import {MatButtonModule} from '@angular/material/button'
+import {MatSnackBarModule} from '@angular/material/snack-bar'
+import {HttpClientModule} from '@angular/common/http'
+
+import {FormsModule} from '@angular/forms'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort'
+import {LOCALE_ID} from '@angular/core'
+import localePt from '@angular/common/locales/pt'
+import {registerLocaleData} from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
+
+registerLocaleData(localePt);
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent,
+    ProductReadComponent,
+    ProductRead2Component,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
@@ -25,9 +58,22 @@ import {MatListModule} from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [{
+      provide:LOCALE_ID,
+      useValue:'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
